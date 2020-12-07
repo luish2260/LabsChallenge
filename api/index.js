82d996b77125747836bd1a3024742192bb0b9180
Maajo Hernández <luis22loco@gmail.com>
2020-12-06 21:48:38 -0300

    a

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const app = express();//y lo estoy empezando a utilizar
 app.use(cors()); //esto es un middleware
 
 app.get('/api/search',(req,res) => {
-  console.log(req.query.q);
+  console.log(req.query.q);//muestro por consola lo que estoy buscando 
   fetch('https://api.mercadolibre.com/sites/MLA/search?q=' + req.query.q)
   .then((res) => res.json())
   .then((data) => {
