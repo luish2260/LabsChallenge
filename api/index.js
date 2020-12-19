03a9c3474c4aab18ff132aab73d10bbd4378257c
Maajo Hernández <luis22loco@gmail.com>
2020-12-19 19:47:50 -0300

    archivos innecesarios eliminados

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
-const express = require('express');//estoy requiriendo el módulo express
-const cors = require('cors');//estoy requiriendo el módulo cors
-const fetch = require("node-fetch");//estoy requiriendo el módulo node fetch
-const app = express();//y lo estoy empezando a utilizar
+const express = require('express'); // estoy requiriendo el módulo express
+const cors = require('cors'); // estoy requiriendo el módulo cors
+const fetch = require('node-fetch'); // estoy requiriendo el módulo node fetch
+const app = express(); // y lo estoy empezando a utilizar
 
-app.use(cors()); //esto es un middleware
+app.use(cors()); // esto es un middleware
 
 app.get('/api/search',(req,res) => {
-  console.log(req.query.q);//muestro por consola lo que estoy buscando 
+  console.log(req.query.q); // muestro por consola lo que estoy buscando 
   fetch('https://api.mercadolibre.com/sites/MLA/search?q=' + req.query.q)
   .then((res) => res.json())
   .then((data) => {
-    res.send(data);
+    res.json(data);
   });
 
 });
