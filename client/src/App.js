78d3d614f580ad2dc8ee3ec4cdae194ffedcfebb
Maajo Hernández <luis22loco@gmail.com>
2020-12-27 22:38:57 -0300

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Catalogo from './components/Catalogo.jsx';
-import Buscador from './components/Buscador.jsx';
-import Footer from './components/Footer.jsx';
-import {Provider} from 'react-redux';
-import generateStore from './redux/store';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-
-  render() {
-
-    const store = generateStore(); // CON ESTO GUARDO LA FUNCION DE GENERAR EL STORE EN UNA CONSTANTE
-
-    return (
-      <Provider store= {store}> {/*EN ESTE SECTOR USO LO DE REDUX, ENCAPSULA TODO EL COMPOONENTE APP*/}
-      <Router>
-      <Route exact path="/" component={Buscador} />
-      <Route path="/" render={() => <Catalogo state={this.state} />} />
-      <Route exact path="/" component={Footer} />
-      </Router>
-      </Provider>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
