66b74779a112e1cc7a8c04a7061c02354936dccb
Maajo Hernández <luis22loco@gmail.com>
2021-01-04 18:05:20 -0300

    a

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,27 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Catalogo from './components/Catalogo.jsx';
 import Buscador from './components/Buscador.jsx';
 import Footer from './components/Footer.jsx';
-import './App.css';
 import { Provider } from 'react-redux';
 import generateStore from './redux/store';
 
 class App extends Component {
- render() {
-  const store = generateStore(); // CON ESTO GUARDO LA FUNCION DE GENERAR EL STORE EN UNA CONSTANTE
-   return (
-      
-    <Provider store= {store}> {/*EN ESTE SECTOR USO LO DE REDUX, ENCAPSULA TODO EL COMPOONENTE APP*/}
-    <Router>
-    <Route exact path="/" component={Buscador} />
-    <Route path="/" render={() => <Catalogo state={this.state} />} />
-    <Route exact path="/" component={Footer} />
-    </Router>
-    </Provider>
+
+render() {
+
+const store = generateStore(); // CON ESTO GUARDO LA FUNCION DE GENERAR EL STORE EN UNA CONSTANTE
    
-   );
- }
-}
+return (
+      
+<Provider store= {store}> {/*EN ESTE SECTOR USO LO DE REDUX, ENCAPSULA TODO EL COMPONENTE APP*/}
+<Router>
+<Route exact path="/" component={Buscador} />
+<Route path="/" render={() => <Catalogo state={this.state} />} />
+<Route exact path="/" component={Footer} />
+</Router>
+</Provider>
+   
+);
+};
+};
 
 export default App;
