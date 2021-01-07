4d393bed8a8eb721f5d814a7310a1bfbe9c51632
Maajo Hernández <luis22loco@gmail.com>
2021-01-07 17:49:44 -0300

    a

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
-const express = require('express'); 
-const cors = require('cors'); 
-const fetch = require('node-fetch'); 
-const app = express(); 
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
+const app = express();
 
-app.use(cors()); 
+app.use(cors());
 
 app.get('/api/search/:q', (req, res) => {
-  console.log('GET request to search'); 
+  console.log('GET request to search');
   fetch('https://api.mercadolibre.com/sites/MLA/search?q=' + req.params.q)
     .then((res) => res.json())
     .then((data) => {
