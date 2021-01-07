c138044fed99c66e4d5c3242c6001a5cc0856d84
Maajo Hernández <luis22loco@gmail.com>
2021-01-07 17:29:10 -0300

    a

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
-const express = require('express'); // estoy requiriendo el módulo express
-const cors = require('cors'); // estoy requiriendo el módulo cors
-const fetch = require('node-fetch'); // estoy requiriendo el módulo node fetch
-const app = express(); // y lo estoy empezando a utilizar
+const express = require('express'); 
+const cors = require('cors'); 
+const fetch = require('node-fetch'); 
+const app = express(); 
 
-app.use(cors()); // esto es un middleware
+app.use(cors()); 
 
 app.get('/api/search/:q', (req, res) => {
-  console.log('GET request to search'); // muestro por consola cada vez que entro a esta ruta
+  console.log('GET request to search'); 
   fetch('https://api.mercadolibre.com/sites/MLA/search?q=' + req.params.q)
     .then((res) => res.json())
     .then((data) => {
