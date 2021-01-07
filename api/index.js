983a7f7fc48c07df3b6384ede37f8991535e03db
Maajo Hernández <luis22loco@gmail.com>
2021-01-07 17:20:31 -0300

    a

--- api/index.js
+++ api/index.js
@@ -6,14 +6,14 @@ const app = express(); // y lo estoy empezando a utilizar
 app.use(cors()); // esto es un middleware
 
 app.get('/api/search/:q', (req, res) => {
- console.log('GET request to search'); // muestro por consola cada vez que entro a esta ruta
- fetch('https://api.mercadolibre.com/sites/MLA/search?q=' + req.params.q)
-  .then((res) => res.json())
-  .then((data) => {
-   res.json(data);
-  });
+  console.log('GET request to search'); // muestro por consola cada vez que entro a esta ruta
+  fetch('https://api.mercadolibre.com/sites/MLA/search?q=' + req.params.q)
+    .then((res) => res.json())
+    .then((data) => {
+      res.json(data);
+    });
 });
 
 app.listen(5000, () => {
- console.log('Server up on port 5000');
+  console.log('Server up on port 5000');
 });
