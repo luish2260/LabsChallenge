c69ffaa3d37d109ff547777c26ff1e0d247c3b52
Maajo Hernández <luis22loco@gmail.com>
2021-02-18 22:05:16 -0300

    a

--- client/src/components/Buscador2.jsx
+++ client/src/components/Buscador2.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { obtenerProductos } from '../redux/actions';
 import { Button } from '@material-ui/core';
